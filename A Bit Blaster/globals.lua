a7a047b12a323868820fbc55ba3e4dcd99cfbecb
Kurt Cabanig <kurtneallorenzcabanig@gmail.com>
2025-08-04 04:49:42 +0800

    Add files via upload

--- A Bit Blaster/globals.lua
+++ A Bit Blaster/globals.lua
@@ -40,7 +40,10 @@ attacks = {
     ['Spin'] = {cooldown = 0.32, ammo = 2, abbreviation = 'Sp', color = hp_color},
     ['Flame'] = {cooldown = 0.048, ammo = 0.4, abbreviation = 'F', color = flame_color},
     ['Bounce'] = {cooldown = 0.32, ammo = 4, abbreviation = 'Bn', color = default_color},
-    ['2Split'] = {cooldown = 0.32, ammo = 3, abbreviation = '2S', color = ammo_color}
+    ['2Split'] = {cooldown = 0.32, ammo = 3, abbreviation = '2S', color = ammo_color},
+    ['4Split'] = {cooldown = 0.4, ammo = 4, abbreviation = '4S', color = boost_color},
+    ['Lightning'] = {cooldown = 0.2, ammo = 8, abbreviation = 'Li', color = default_color},
+    ['Explode'] = {cooldown = 0.6, ammo = 4, abbreviation = 'E', color = hp_color}
 }
 
 change_attack = {
@@ -49,7 +52,8 @@ change_attack = {
     "Back", "Side", 
     "Homing", "Blast", 
     "Spin", "Flame",
-    "Bounce", "2Split"
+    "Bounce", "2Split",
+    "4Split"
 }
 
 enemies = {'Rock', 'Shooter'}
